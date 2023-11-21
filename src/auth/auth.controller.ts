056fa317420e9e22044d79d281906f613e075159
Gan-Xing <599153574@qq.com>
2023-11-21 14:16:49 +0800

    测试Redis模块

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,13 @@
 // src/auth/auth.controller.ts
+import { Body, Controller, Post, HttpCode, HttpStatus, Request, Get } from '@nestjs/common';
 import {
-  Body,
-  Controller,
-  Post,
-  HttpCode,
-  HttpStatus,
-  Request,
-} from '@nestjs/common';
-import {
-  ApiBadRequestResponse,
-  ApiOkResponse,
-  ApiTags,
-  ApiUnauthorizedResponse,
-  ApiBearerAuth,
-  ApiNotFoundResponse,
-  ApiCreatedResponse,
+	ApiBadRequestResponse,
+	ApiOkResponse,
+	ApiTags,
+	ApiUnauthorizedResponse,
+	ApiBearerAuth,
+	ApiNotFoundResponse,
+	ApiCreatedResponse
 } from '@nestjs/swagger';
 import { Public } from '../common';
 import { AuthService } from './auth.service';
@@ -23,37 +16,43 @@ import { LoginDto, RefreshTokenDto, RegisterDto, Token } from './dto';
 @Controller('api/auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private readonly auth: AuthService) {}
+	constructor(private readonly auth: AuthService) {}
 
-  @Public()
-  @Post('login')
-  @ApiOkResponse({ type: Token })
-  @ApiBadRequestResponse({ description: 'Invalid request body' })
-  @ApiUnauthorizedResponse({ description: 'Invalid password' })
-  @ApiNotFoundResponse({ description: 'No user found for email' })
-  login(@Body() { email, password }: LoginDto) {
-    return this.auth.login(email, password);
-  }
+	@Public()
+	@Get('test-redis')
+	async testRedis() {
+		return this.auth.testRedis();
+	}
 
-  @Post('logout')
-  @ApiBearerAuth()
-  @HttpCode(HttpStatus.OK)
-  logout(@Request() req): Promise<boolean> {
-    const id = req.user.id;
-    return this.auth.logout(id);
-  }
+	@Public()
+	@Post('login')
+	@ApiOkResponse({ type: Token })
+	@ApiBadRequestResponse({ description: 'Invalid request body' })
+	@ApiUnauthorizedResponse({ description: 'Invalid password' })
+	@ApiNotFoundResponse({ description: 'No user found for email' })
+	login(@Body() { email, password }: LoginDto) {
+		return this.auth.login(email, password);
+	}
 
-  @Public()
-  @Post('register')
-  @ApiCreatedResponse({ type: Token })
-  @ApiBadRequestResponse({ description: 'Invalid request body' })
-  async register(@Body() createUserDto: RegisterDto) {
-    return this.auth.register(createUserDto);
-  }
-  @Post('refresh')
-  @ApiBearerAuth()
-  @ApiCreatedResponse({ description: 'Refresh user token.' })
-  async refresh(@Body('refreshToken') { refreshToken }: RefreshTokenDto) {
-    return this.auth.refreshToken(refreshToken);
-  }
+	@Post('logout')
+	@ApiBearerAuth()
+	@HttpCode(HttpStatus.OK)
+	logout(@Request() req): Promise<boolean> {
+		const id = req.user.id;
+		return this.auth.logout(id);
+	}
+
+	@Public()
+	@Post('register')
+	@ApiCreatedResponse({ type: Token })
+	@ApiBadRequestResponse({ description: 'Invalid request body' })
+	async register(@Body() createUserDto: RegisterDto) {
+		return this.auth.register(createUserDto);
+	}
+	@Post('refresh')
+	@ApiBearerAuth()
+	@ApiCreatedResponse({ description: 'Refresh user token.' })
+	async refresh(@Body('refreshToken') { refreshToken }: RefreshTokenDto) {
+		return this.auth.refreshToken(refreshToken);
+	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,194 +1,205 @@
 //src/auth/auth.service.ts
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import { JwtConfig, SecurityConfig } from 'src/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PasswordService } from 'src/password/password.service';
+import { RedisService } from 'src/redis/redis.service';
 import { RegisterDto, Token } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly prisma: PrismaService,
-    private readonly passwordService: PasswordService,
-    private readonly configService: ConfigService,
-  ) {}
-  async register(registerUser: RegisterDto): Promise<Token> {
-    // 确保邮箱是唯一的
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email: registerUser.email.toLowerCase() },
-    });
-    if (existingUser) {
-      throw new Error('Email already in use');
-    }
+	constructor(
+		private readonly jwtService: JwtService,
+		private readonly prisma: PrismaService,
+		private readonly passwordService: PasswordService,
+		private readonly configService: ConfigService,
+		private readonly redisService: RedisService
+	) {}
+	// 测试 Redis 的方法
+	async testRedis(): Promise<void> {
+		// 使用默认客户端
+		await this.redisService.set('key1', 'value1');
+		const value1 = await this.redisService.get('key1');
+		console.log(`Value from default client: ${value1}`);
 
-    const hashedPassword = await this.passwordService.hashPassword(
-      registerUser.password,
-    );
+		// // 使用次要客户端
+		// await this.redisService.set('key2', 'value2', 'secondary');
+		// const value2 = await this.redisService.get('key2', 'secondary');
+		// console.log(`Value from secondary client: ${value2}`);
+		// return this.redisClient.get('testKey');
+	}
+	async register(registerUser: RegisterDto): Promise<Token> {
+		// 确保邮箱是唯一的
+		const existingUser = await this.prisma.user.findUnique({
+			where: { email: registerUser.email.toLowerCase() }
+		});
+		if (existingUser) {
+			throw new Error('Email already in use');
+		}
 
-    // 设定默认的用户角色
-    const defaultRole = await this.prisma.role.findUnique({ where: { id: 2 } });
-    if (!defaultRole) {
-      throw new Error('Default role does not exist');
-    }
+		const hashedPassword = await this.passwordService.hashPassword(
+			registerUser.password
+		);
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: registerUser.email.toLowerCase(),
-        password: hashedPassword,
-        roles: {
-          connect: [{ id: defaultRole.id }], // 连接到默认角色
-        },
-        status: '1',
-        username: registerUser.username,
-        // 根据你的业务逻辑设定默认的gender和departmentId
-        gender: '1',
-        departmentId: 1,
-      },
-    });
+		// 设定默认的用户角色
+		const defaultRole = await this.prisma.role.findUnique({ where: { id: 2 } });
+		if (!defaultRole) {
+			throw new Error('Default role does not exist');
+		}
 
-    // Return a JWT
-    const tokens = await this.generateTokens({ userId: user.id });
-    await this.updateRtHash(user.id, tokens.refreshToken);
+		const user = await this.prisma.user.create({
+			data: {
+				email: registerUser.email.toLowerCase(),
+				password: hashedPassword,
+				roles: {
+					connect: [{ id: defaultRole.id }] // 连接到默认角色
+				},
+				status: '1',
+				username: registerUser.username,
+				// 根据你的业务逻辑设定默认的gender和departmentId
+				gender: '1',
+				departmentId: 1
+			}
+		});
 
-    return tokens;
-  }
+		// Return a JWT
+		const tokens = await this.generateTokens({ userId: user.id });
+		await this.updateRtHash(user.id, tokens.refreshToken);
 
-  async login(email: string, password: string): Promise<Token> {
-    // Step 1: Fetch a user with the given email
-    const user = await this.prisma.user.findUnique({
-      where: { email: email.toLowerCase() },
-    });
+		return tokens;
+	}
 
-    // If no user is found, throw an error
-    if (!user) {
-      throw new NotFoundException(`No user found for email: ${email}`);
-    }
+	async login(email: string, password: string): Promise<Token> {
+		// Step 1: Fetch a user with the given email
+		const user = await this.prisma.user.findUnique({
+			where: { email: email.toLowerCase() }
+		});
 
-    // Step 2: Check if the password is correct
-    const isPasswordValid = await this.passwordService.validatePassword(
-      password,
-      user.password,
-    );
+		// If no user is found, throw an error
+		if (!user) {
+			throw new NotFoundException(`No user found for email: ${email}`);
+		}
 
-    // If password does not match, throw an error
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid password');
-    }
+		// Step 2: Check if the password is correct
+		const isPasswordValid = await this.passwordService.validatePassword(
+			password,
+			user.password
+		);
 
-    // Step 3: Generate a JWT containing the user's ID and return it
-    const tokens = await this.generateTokens({ userId: user.id });
-    await this.updateRtHash(user.id, tokens.refreshToken);
-    return tokens;
-  }
+		// If password does not match, throw an error
+		if (!isPasswordValid) {
+			throw new UnauthorizedException('Invalid password');
+		}
 
-  async logout(userId: number): Promise<boolean> {
-    await this.prisma.user.updateMany({
-      where: {
-        id: userId,
-        hashedRt: {
-          not: null,
-        },
-      },
-      data: {
-        hashedRt: null,
-      },
-    });
-    return true;
-  }
+		// Step 3: Generate a JWT containing the user's ID and return it
+		const tokens = await this.generateTokens({ userId: user.id });
+		await this.updateRtHash(user.id, tokens.refreshToken);
+		return tokens;
+	}
 
-  private generateAccessToken(payload: { userId: number }) {
-    const securityConfig = this.configService.get<SecurityConfig>('security');
-    const token = this.jwtService.sign(payload);
-    const expiresIn = this.convertExpiresInToSeconds(securityConfig.expiresIn);
-    return {
-      token,
-      expiresIn,
-    };
-  }
+	async logout(userId: number): Promise<boolean> {
+		await this.prisma.user.updateMany({
+			where: {
+				id: userId,
+				hashedRt: {
+					not: null
+				}
+			},
+			data: {
+				hashedRt: null
+			}
+		});
+		return true;
+	}
 
-  private generateRefreshToken(payload: { userId: number }) {
-    const securityConfig = this.configService.get<SecurityConfig>('security');
-    const jwtConfig = this.configService.get<JwtConfig>('jwt');
+	private generateAccessToken(payload: { userId: number }) {
+		const securityConfig = this.configService.get<SecurityConfig>('security');
+		const token = this.jwtService.sign(payload);
+		const expiresIn = this.convertExpiresInToSeconds(securityConfig.expiresIn);
+		return {
+			token,
+			expiresIn
+		};
+	}
 
-    const token = this.jwtService.sign(payload, {
-      secret: jwtConfig.refreshSecret,
-      expiresIn: securityConfig.refreshIn,
-    });
-    const expiresIn = this.convertExpiresInToSeconds(securityConfig.refreshIn);
+	private generateRefreshToken(payload: { userId: number }) {
+		const securityConfig = this.configService.get<SecurityConfig>('security');
+		const jwtConfig = this.configService.get<JwtConfig>('jwt');
 
-    return {
-      token,
-      expiresIn,
-    };
-  }
+		const token = this.jwtService.sign(payload, {
+			secret: jwtConfig.refreshSecret,
+			expiresIn: securityConfig.refreshIn
+		});
+		const expiresIn = this.convertExpiresInToSeconds(securityConfig.refreshIn);
 
-  async refreshToken(token: string) {
-    const jwtConfig = this.configService.get<JwtConfig>('jwt');
-    const { userId } = this.jwtService.verify(token, {
-      secret: jwtConfig.refreshSecret,
-    });
-    const user = await this.validateUser(userId);
-    const isJwtValid = await this.passwordService.validatePassword(
-      token,
-      user.hashedRt,
-    );
-    if (!user || !isJwtValid) {
-      throw new UnauthorizedException();
-    }
-    const tokens = await this.generateTokens({ userId });
-    await this.updateRtHash(userId, tokens.refreshToken);
-    return tokens;
-  }
+		return {
+			token,
+			expiresIn
+		};
+	}
 
-  validateUser(userId: number): Promise<User> {
-    return this.prisma.user.findUnique({ where: { id: userId } });
-  }
+	async refreshToken(token: string) {
+		const jwtConfig = this.configService.get<JwtConfig>('jwt');
+		const { userId } = this.jwtService.verify(token, {
+			secret: jwtConfig.refreshSecret
+		});
+		const user = await this.validateUser(userId);
+		const isJwtValid = await this.passwordService.validatePassword(
+			token,
+			user.hashedRt
+		);
+		if (!user || !isJwtValid) {
+			throw new UnauthorizedException();
+		}
+		const tokens = await this.generateTokens({ userId });
+		await this.updateRtHash(userId, tokens.refreshToken);
+		return tokens;
+	}
 
-  getUserFromToken(token: string): Promise<User> {
-    const id = this.jwtService.decode(token)['userId'];
-    return this.prisma.user.findUnique({ where: { id } });
-  }
+	validateUser(userId: number): Promise<User> {
+		return this.prisma.user.findUnique({ where: { id: userId } });
+	}
 
-  generateTokens(payload: { userId: number }): Token {
-    const accessToken = this.generateAccessToken(payload);
-    const refreshToken = this.generateRefreshToken(payload);
+	getUserFromToken(token: string): Promise<User> {
+		const id = this.jwtService.decode(token)['userId'];
+		return this.prisma.user.findUnique({ where: { id } });
+	}
 
-    return {
-      accessToken: accessToken.token,
-      refreshToken: refreshToken.token,
-      accessExpiresIn: accessToken.expiresIn,
-      refreshExpiresIn: refreshToken.expiresIn,
-    };
-  }
+	generateTokens(payload: { userId: number }): Token {
+		const accessToken = this.generateAccessToken(payload);
+		const refreshToken = this.generateRefreshToken(payload);
 
-  private async updateRtHash(userId: number, rt: string): Promise<void> {
-    const hash = await this.passwordService.hashPassword(rt);
-    await this.prisma.user.update({
-      where: { id: userId },
-      data: { hashedRt: hash },
-    });
-  }
+		return {
+			accessToken: accessToken.token,
+			refreshToken: refreshToken.token,
+			accessExpiresIn: accessToken.expiresIn,
+			refreshExpiresIn: refreshToken.expiresIn
+		};
+	}
 
-  convertExpiresInToSeconds(expiresIn: string): number {
-    const expiresInNumber = parseInt(expiresIn);
-    if (expiresIn.endsWith('s')) {
-      return expiresInNumber;
-    } else if (expiresIn.endsWith('m')) {
-      return expiresInNumber * 60;
-    } else if (expiresIn.endsWith('h')) {
-      return expiresInNumber * 60 * 60;
-    } else if (expiresIn.endsWith('d')) {
-      return expiresInNumber * 60 * 60 * 24;
-    } else {
-      throw new Error(`Unable to parse expiresIn value: ${expiresIn}`);
-    }
-  }
+	private async updateRtHash(userId: number, rt: string): Promise<void> {
+		const hash = await this.passwordService.hashPassword(rt);
+		await this.prisma.user.update({
+			where: { id: userId },
+			data: { hashedRt: hash }
+		});
+	}
+
+	convertExpiresInToSeconds(expiresIn: string): number {
+		const expiresInNumber = parseInt(expiresIn);
+		if (expiresIn.endsWith('s')) {
+			return expiresInNumber;
+		} else if (expiresIn.endsWith('m')) {
+			return expiresInNumber * 60;
+		} else if (expiresIn.endsWith('h')) {
+			return expiresInNumber * 60 * 60;
+		} else if (expiresIn.endsWith('d')) {
+			return expiresInNumber * 60 * 60 * 24;
+		} else {
+			throw new Error(`Unable to parse expiresIn value: ${expiresIn}`);
+		}
+	}
 }
